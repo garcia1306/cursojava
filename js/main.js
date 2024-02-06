887747fab2c5d861d840849f4305aec7222df234
garcia1306 <tomasgm05@gmail.com>
2024-02-06 17:52:58 -0300

    1er version

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let ingreso = parseInt(prompt("Ingresa la cantidad de remeras que deseas comprar")) ;
+
+for (let i = 15; i <= 15; i++){
+let resultado = ingreso * i
+
+
+
+console.log("el precio que debes pagar es $" + resultado);
+
+alert("el precio que debes pagar es $" + resultado)
+
+
+
+
+}
+
+
+let ingresatalle = prompt("Ingresa el talle que quieres llevar")
+let talles = "S s M m L l XL xl XXL xxl"
+
+if( ingresotalle = talles){
+    alert("estamos preparando tu pedido");
+}
+else{
+    alert("talle no encontrado")
+}
